5027427364819ed6716c5b69bf674245949fd33a
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:31:28 +1300

    PATCH: module clean-up

---
+++ tests/SortableListViewTest.php
@@ -0,0 +1,13 @@
+<?php
+class SortableListViewTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
